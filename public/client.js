97d3bc7d5215b902092e24e71c085d4980b1c2d1
c05m4r <Marcos Gabriel Miller>
2024-11-10 09:56:47 -0300

    completado parte3

--- public/client.js
+++ public/client.js
@@ -1,11 +1,23 @@
 $(document).ready(function () {
   let socket = io();
-  socket.on('user count', function (data) {
-    console.log(data);
+
+  socket.on('user', data => {
+      $('#num-users').text(data.currentUsers + ' users online');
+      let message =
+          data.username +
+          (data.connected ? ' has joined the chat.' : ' has left the chat.');
+      $('#messages').append($('<li>').html('<b>' + message + '</b>'));
   });
+
+  socket.on('chat message', data => {
+      console.log('socket.on 1')
+      $('#messages').append($('<li>').text(`${data.username}: ${data.message}`));
+  })
+
   $('form').submit(function () {
-    var messageToSend = $('#m').val();
-    $('#m').val('');
-    return false;
+      let messageToSend = $('#m').val();
+      socket.emit('chat message', messageToSend);
+      $('#m').val('');
+      return false; // prevent form submit from refreshing page
   });
 });
--- server.js
+++ server.js
@@ -12,6 +12,11 @@ const app = express();
 
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
+const passportSocketIo = require('passport.socketio');
+const cookieParser = require('cookie-parser');
+const MongoStore = require('connect-mongo')(session);
+const URI = process.env.MONGO_URI;
+const store = new MongoStore({ url: URI });
 
 app.set('view engine', 'pug');
 app.set('views', './views/pug');
@@ -31,14 +36,46 @@ app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+io.use(
+  passportSocketIo.authorize({
+    cookieParser: cookieParser,
+    key: 'express.sid',
+    secret: process.env.SESSION_SECRET,
+    store: store,
+    success: onAuthorizeSuccess,
+    fail: onAuthorizeFail
+  })
+);
+
 myDB(async client => {
   const myDataBase = await client.db('database').collection('users');
 
   routes(app, myDataBase);
   auth(app, myDataBase);
 
-  io.on('connection', socket => {
+  let currentUsers = 0;
+  io.on('connection', (socket) => {
+    ++currentUsers;
+    io.emit('user', {
+      username: socket.request.user.username,
+      currentUsers,
+      connected: true
+    });
     console.log('A user has connected');
+
+    socket.on('chat message', (message) => {
+      io.emit('chat message', { username: socket.request.user.username, message });
+    });
+
+    socket.on('disconnect', () => {
+      --currentUsers;
+      io.emit('user', {
+        username: socket.request.user.username,
+        currentUsers,
+        connected: false
+      });
+      console.log('A user has disconnected');
+    });
   });
 
 }).catch(e => {
@@ -47,6 +84,17 @@ myDB(async client => {
   });
 });
   
+function onAuthorizeSuccess(data, accept) {
+  console.log('successful connection to socket.io');
+  accept(null, true);
+}
+
+function onAuthorizeFail(data, message, error, accept) {
+  if (error) throw new Error(message);
+  console.log('failed connection to socket.io:', message);
+  accept(null, false);
+}
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
